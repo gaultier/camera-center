81cebd1c131a2515f3b0de9d00eb1338f5e49de8
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-05-26 16:26:28 +0200

    [wip]

--- sender-zig/src/main.zig
+++ sender-zig/src/main.zig
@@ -1,18 +1,47 @@
 const std = @import("std");
 
-// const State = enum {
-//     Idle,
-//     MotionStarted,
-//     MotionStopped,
-// };
+const State = enum {
+    None,
+    MotionStarted,
+    MotionStopped,
+};
 
-// const ParseState = enum { None, SeenMotion };
+const ParseState = enum { None, SeenMotion };
+
+fn parse_child_output(in: std.fs.File) !void {
+    var read_buf = [_]u8{0} ** 1024;
+
+    var state = State.None;
+    var parse_state = ParseState.None;
+    while (true) {
+        if (std.posix.read(in.handle, &read_buf)) |read| {
+            std.debug.print("stderr len={} read={s}\n", .{ read, read_buf[0..read] });
+
+            const stderr = read_buf[0..read];
+            var it = std.mem.splitAny(u8, stderr, "\n ");
+
+            while (it.next()) |word| {
+                if (std.mem.eql(u8, word, "Motion")) {
+                    parse_state = ParseState.SeenMotion;
+                } else if (parse_state == ParseState.SeenMotion and std.mem.eql(u8, word, "detected")) {
+                    state = State.MotionStarted;
+                    std.debug.print("state=read frame data\n", .{});
+                    parse_state = ParseState.None;
+                } else if (parse_state == ParseState.SeenMotion and std.mem.eql(u8, word, "stopped")) {
+                    state = State.MotionStopped;
+                    std.debug.print("state=idle\n", .{});
+                    parse_state = ParseState.None;
+                }
+            }
+        } else |err| {
+            std.debug.print("stderr read error {}\n", .{err});
+        }
+    }
+}
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
-
-    // const socket = std.posix.socket(std.posix.AF.INET, std.posix.SOCK.DGRAM, 0) catch unreachable;
 
     var destination_address: []const u8 = undefined;
     var args = std.process.args();
@@ -20,11 +49,6 @@ pub fn main() !void {
     if (args.next()) |arg| {
         destination_address = arg;
     }
-    // _ = std.net.Address.parseIp4(destination_address, 12345) catch unreachable;
-    // std.posix.connect(socket, &parsed_address.any, parsed_address.getOsSockLen()) catch |err| {
-    //     std.debug.print("failed to connect socket {}", .{err});
-    //     std.posix.exit(1);
-    // };
 
     const allocator = arena.allocator();
     const udp_address: []const u8 = try std.mem.concat(allocator, u8, &[2][]const u8{ "udp://", destination_address });
@@ -53,73 +77,11 @@ pub fn main() !void {
         udp_address,
     };
     var child = std.ChildProcess.init(&child_args, allocator);
-    // child.stderr_behavior = std.ChildProcess.StdIo.Pipe;
-    // child.stdout = std.fs.File{ .handle = socket };
-
-    // var stderr_buf = [_]u8{0} ** 1024;
+    child.stderr_behavior = std.ChildProcess.StdIo.Pipe;
 
     try child.spawn();
+    try parse_child_output(child.stderr.?);
+
     const term = try child.wait();
     std.debug.print("child wait {}", .{term});
-
-    // var poll_fds = [2]std.posix.pollfd{ .{
-    //     .fd = child.stderr.?.handle,
-    //     .events = std.posix.POLL.IN,
-    //     .revents = 0,
-    // }, .{
-    //     .fd = child.stdout.?.handle,
-    //     .events = std.posix.POLL.IN,
-    //     .revents = 0,
-    // } };
-
-    // var state = State.Idle;
-    // var parse_state = ParseState.None;
-
-    // while (true) {
-    //     const res = std.posix.poll(&poll_fds, -1) catch |err| {
-    //         std.debug.print("poll error {}\n", .{err});
-    //         continue;
-    //     };
-    //     std.debug.assert(res != 0);
-
-    //     if ((poll_fds[0].revents & std.posix.POLL.IN) != 0) {
-    //         if (std.posix.read(poll_fds[0].fd, &stderr_buf)) |read| {
-    //             std.debug.print("stderr len={} read={s}\n", .{ read, stderr_buf[0..read] });
-
-    //             const stderr = stderr_buf[0..read];
-    //             var it = std.mem.splitAny(u8, stderr, "\n ");
-
-    //             while (it.next()) |word| {
-    //                 if (std.mem.eql(u8, word, "Motion")) {
-    //                     parse_state = ParseState.SeenMotion;
-    //                 } else if (parse_state == ParseState.SeenMotion and std.mem.eql(u8, word, "detected")) {
-    //                     state = State.MotionStarted;
-    //                     std.debug.print("state=read frame data\n", .{});
-    //                     parse_state = ParseState.None;
-    //                 } else if (parse_state == ParseState.SeenMotion and std.mem.eql(u8, word, "stopped")) {
-    //                     state = State.MotionStopped;
-    //                     std.debug.print("state=idle\n", .{});
-    //                     parse_state = ParseState.None;
-    //                 }
-    //             }
-    //         } else |err| {
-    //             std.debug.print("stderr read error {}\n", .{err});
-    //         }
-    //     }
-
-    //     if ((poll_fds[1].revents & std.posix.POLL.IN) != 0) {
-    //         if (std.posix.read(poll_fds[1].fd, &stdout_buf)) |read| {
-    //             const message = stdout_buf[0..read];
-    //             _ = message;
-
-    //             if (state == State.MotionStarted) {
-    //                 // TODO
-    //             } else if (state == State.MotionStopped) {
-    //                 // TODO
-    //             }
-    //         } else |err| {
-    //             std.debug.print("stdout read error {}\n", .{err});
-    //         }
-    //     }
-    // }
 }
