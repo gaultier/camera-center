a44539d0da2ddb1f7e790428ec3d1459c8fec705
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-05-26 07:39:58 +0200

    [wip]

--- src/main.zig
+++ src/main.zig
@@ -7,4 +7,34 @@ pub fn main() !void {
         std.debug.print("failed to bind {}", .{err});
         return;
     };
+
+    std.posix.listen(socket, 4) catch |err| {
+        std.debug.print("failed to listen {}", .{err});
+        return;
+    };
+
+    var poll_fds = [_]std.posix.pollfd{
+        .{
+            .fd = socket,
+            .events = std.posix.POLL.IN,
+            .revents = 0,
+        },
+    };
+
+    var read_buf = [_]u8{0} ** (1 << 16);
+
+    while (true) {
+        _ = std.posix.poll(&poll_fds, -1) catch |err| {
+            std.debug.print("failed to poll {}", .{err});
+            continue;
+        };
+
+        if ((poll_fds[0].revents & std.posix.POLL.IN) != 0) {
+            if (std.posix.read(poll_fds[0].fd, &read_buf)) |n| {
+                std.debug.print("read {}", .{n});
+            } else |err| {
+                std.debug.print("failed to read {}", .{err});
+            }
+        }
+    }
 }
