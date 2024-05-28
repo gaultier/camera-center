5ed01cbecbceea5c9c3851cf46af77b934d8bf2e
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-05-28 08:44:35 +0200

    bind udp socket

--- server/src/main.zig
+++ server/src/main.zig
@@ -5,12 +5,32 @@ fn handle_connection(connection: *std.net.Server.Connection) !void {
     while (true) {
         var read_buffer = [_]u8{0} ** 4096;
         const read = try connection.stream.read(&read_buffer);
-        std.debug.print("read={} {x}\n", .{ read, read_buffer[0..read] });
+        std.debug.print("tcp read={} {x}\n", .{ read, read_buffer[0..read] });
+        if (read == 0) {
+            std.debug.print("tcp read={} client likely closed the connection\n", .{read});
+            std.process.exit(0);
+        }
+    }
+}
+
+fn listen_udp() !void {
+    const udp_socket = try std.posix.socket(std.posix.AF.INET, std.posix.SOCK.DGRAM, 0);
+    const address = std.net.Address.parseIp4("0.0.0.0", 12345) catch unreachable;
+    try std.posix.bind(udp_socket, &address.any, address.getOsSockLen());
+
+    while (true) {
+        var read_buffer = [_]u8{0} ** 4096;
+        if (std.posix.read(udp_socket, &read_buffer)) |n| {
+            std.debug.print("udp read={}\n", .{n});
+        } else |err| {
+            std.debug.print("failed to read udp {}\n", .{err});
+        }
     }
 }
 
 pub fn main() !void {
-    // const udp_socket = std.posix.socket(std.posix.AF.INET, std.posix.SOCK.DGRAM, 0) catch unreachable;
+    _ = try std.Thread.spawn(.{}, listen_udp, .{});
+
     const address = std.net.Address.parseIp4("0.0.0.0", 12345) catch unreachable;
     var server = try std.net.Address.listen(address, .{ .reuse_address = true });
     while (true) {
