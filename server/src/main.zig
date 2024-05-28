a9b5ac7d21470d4789852ba0c66cf67ba3aaa2b8
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-05-28 08:34:21 +0200

    trim

--- server/src/main.zig
+++ server/src/main.zig
@@ -1,89 +1,25 @@
 const std = @import("std");
 const root = @import("./root");
 
-pub fn main() !void {
-    const socket = std.posix.socket(std.posix.AF.INET, std.posix.SOCK.STREAM, 0) catch unreachable;
-    const address = std.net.Address.parseIp4("0.0.0.0", 12345) catch unreachable;
-
-    std.posix.bind(socket, &address.any, address.getOsSockLen()) catch |err| {
-        std.debug.print("failed to bind {}\n", .{err});
-        return;
-    };
-
-    std.posix.listen(socket, 16) catch |err| {
-        std.debug.print("failed to listen {}\n", .{err});
-        return;
-    };
-
-    var allocator_memory = [_]u8{0} ** 256;
-    var allocator = std.heap.FixedBufferAllocator.init(&allocator_memory);
-    var poll_fds = std.ArrayList(std.posix.pollfd).init(allocator.allocator());
-    var read_buf = [_]u8{0} ** (1 << 16);
-
-    var peer_address: std.net.Address = undefined;
-    var addr_len: std.posix.socklen_t = @sizeOf(std.net.Address);
-    var client_fd: i32 = 0;
-    if (std.posix.accept(socket, &peer_address.any, &addr_len, 0)) |fd| {
-        client_fd = fd;
-    } else |err| {
-        std.debug.print("failed to accept {}\n", .{err});
-        return;
-    }
-    poll_fds.append(.{ .fd = client_fd, .events = std.posix.POLL.IN, .revents = 0 }) catch |err| {
-        std.debug.print("failed to add client_fd {}", .{err});
-        return;
-    };
-
-    var file: std.posix.fd_t = 0;
-    if (std.posix.open("out.ts", std.posix.O{ .ACCMODE = .WRONLY, .CREAT = true, .APPEND = true }, 0o600)) |fd| {
-        file = fd;
-    } else |err| {
-        std.debug.print("failed to open file {}\n", .{err});
-        return;
-    }
-
+fn handle_connection(connection: *std.net.Server.Connection) !void {
     while (true) {
-        _ = std.posix.poll(poll_fds.items, -1) catch |err| {
-            std.debug.print("failed to poll {}\n", .{err});
+        var read_buffer = [_]u8{0} ** 4096;
+        const read = try connection.stream.read(&read_buffer);
+        std.debug.print("read={} {x}\n", .{ read, read_buffer[0..read] });
+    }
+}
+
+pub fn main() !void {
+    // const udp_socket = std.posix.socket(std.posix.AF.INET, std.posix.SOCK.DGRAM, 0) catch unreachable;
+    const address = std.net.Address.parseIp4("0.0.0.0", 12345) catch unreachable;
+    var server = try std.net.Address.listen(address, .{ .reuse_address = true });
+    while (true) {
+        var connection = try server.accept();
+        const pid = try std.posix.fork();
+        if (pid > 0) { // Parent.
             continue;
-        };
-
-        var i: u64 = 0;
-        while (i < poll_fds.items.len) {
-            const poll_fd = poll_fds.items[i];
-
-            if ((poll_fd.revents & std.posix.POLL.ERR) != 0) {
-                std.debug.print("client err\n", .{});
-                std.posix.close(client_fd);
-                _ = poll_fds.swapRemove(i);
-                continue;
-            }
-
-            if ((poll_fd.revents & std.posix.POLL.IN) != 0) {
-                if (std.posix.read(poll_fd.fd, &read_buf)) |read| {
-                    if (read == 0) {
-                        std.debug.print("client closed\n", .{});
-                        std.posix.close(client_fd);
-                        _ = poll_fds.swapRemove(i);
-                        continue;
-                    }
-
-                    std.debug.print("read {}\n", .{read});
-
-                    if (std.posix.write(file, read_buf[0..read])) |written| {
-                        std.debug.print("written {}\n", .{written});
-                    } else |err| {
-                        std.debug.print("failed to write {}\n", .{err});
-                        i += 1;
-                        continue;
-                    }
-                } else |err| {
-                    std.debug.print("failed to read {}\n", .{err});
-                    i += 1;
-                    continue;
-                }
-            }
-            i += 1;
         }
+        // Child
+        try handle_connection(&connection);
     }
 }
