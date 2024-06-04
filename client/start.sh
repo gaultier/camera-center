96700e79f7207998115149cfe23d6c79f0d7a5b4
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-06-04 20:12:29 +0200

    start.sh

--- client/start.sh
+++ client/start.sh
@@ -2,4 +2,4 @@
 set -ex
 set -o pipefail
 
-rpicam-vid -t 0 --inline  --width 1920 --height 1080 --hdr -n   --bitrate 2000000 --post-process-file=motion_detect.json --lores-width 128 --lores-height 128  2> >(./client $1 $2 ) -o udp://239.0.0.1:12345  --codec libav --libav-format mpegts
+rpicam-vid -t 0 --inline  --width 1920 --height 1080 --hdr -n   --bitrate 2000000 --post-process-file=motion_detect.json --lores-width 128 --lores-height 128  2> >(./client $1 $2 ) -o udp://$1:$2  --codec libav --libav-format mpegts
