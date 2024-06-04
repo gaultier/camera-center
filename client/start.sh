1942af9609cbe7b85cc176f12dfc30bdfaf7f27f
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-06-04 11:03:42 +0200

    start.sh

---
+++ client/start.sh
@@ -0,0 +1,1 @@
+rpicam-vid -t 0 --inline  --width 1920 --height 1080 -o - --hdr -n   --bitrate 1000000 --post-process-file=motion_detect.json --lores-width 128 --lores-height 128  2> >(./client 192.168.1.120 12345 ) | ffmpeg -re -i /dev/stdin -c copy -f mpegts  'udp://239.0.0.1:12345?ttl=13&pkt_size=1316'  -hide_banner -loglevel error
