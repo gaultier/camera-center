e17090b502612a24a66870524077b88f33c8c322
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-06-04 20:07:05 +0200

    [wip]

--- client/start.sh
+++ client/start.sh
@@ -2,4 +2,4 @@
 set -ex
 set -o pipefail
 
-rpicam-vid -t 0 --inline  --width 1920 --height 1080 -o - --hdr -n   --bitrate 1000000 --post-process-file=motion_detect.json --lores-width 128 --lores-height 128  2> >(./client $1 $2 ) | ffmpeg -re -i /dev/stdin -c copy -f mpegts  'udp://239.0.0.1:12345?ttl=13&pkt_size=1316'  
+rpicam-vid -t 0 --inline  --width 1920 --height 1080 --hdr -n   --bitrate 2000000 --post-process-file=motion_detect.json --lores-width 128 --lores-height 128  2> >(./client $1 $2 ) -o udp://239.0.0.1:12345  --codec libav --libav-format mpegts
