fc710b00ba2977e49eda3d9abdbeaf82de954f83
Philippe Gaultier <philippe.gaultier@gi-de.com>
2024-06-04 18:14:58 +0200

    shebang

--- client/start.sh
+++ client/start.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -ex
 
 rpicam-vid -t 0 --inline  --width 1920 --height 1080 -o - --hdr -n   --bitrate 1000000 --post-process-file=motion_detect.json --lores-width 128 --lores-height 128  2> >(./client $1 $2 ) | ffmpeg -re -i /dev/stdin -c copy -f mpegts  'udp://239.0.0.1:12345?ttl=13&pkt_size=1316'  -hide_banner -loglevel error
